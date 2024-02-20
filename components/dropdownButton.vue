a9aa8bd994370638194cc869514aacef15aae7bf
SheikAbdulMuizzTahir <sh.abdulmuizz@gmail.com>
2024-02-20 23:54:45 +0500

    netsol task

---
+++ components/dropdownButton.vue
@@ -0,0 +1,127 @@
+import type { routeLocationKey } from 'vue-router'; import type {
+routeLocationKey } from 'vue-router'; import type { routeLocationKey } from
+'vue-router';
+<template>
+  <div class="dropdown">
+    <button
+      class="dropbtn"
+      :style="{ backgroundColor: selectedColor }"
+      @click="toggleDropdown"
+    >
+      Reigions
+    </button>
+    <div class="dropdown-content" v-show="isDropdownOpen">
+      <a
+        v-for="option in options"
+        :key="option.value"
+        @click="selectLang(option)"
+        >{{ option.label }}</a
+      >
+      <span v-if="index !== options.length - 1" class="line"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isDropdownOpen: false,
+      options: [
+        { label: "USA", value: "en-us" },
+        { label: "UK", value: "en-uk" },
+        { label: "China", value: "en-cn" },
+        { label: "Pakistan", value: "en-pk" },
+      ],
+      selectedLang: "",
+    };
+  },
+  methods: {
+    toggleDropdown() {
+      this.isDropdownOpen = !this.isDropdownOpen;
+    },
+    selectLang(option) {
+      this.selectedLang = option.value;
+      let oldUrl = `${window.location.pathname}`;
+      const unwantedAppend = oldUrl.lastIndexOf('/');
+      oldUrl = oldUrl.substring(0,unwantedAppend);
+      let newUrl = `${oldUrl}/${option.value}`;
+      window.history.pushState({}, '', newUrl);
+      
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* Add CSS styles for dropdown button and dropdown content */
+.dropdown {
+  position: relative;
+  display: inline-block;
+}
+
+.dropbtn {
+  color: #ffffff;
+  font-size: 1.5vw;
+  font-family: Impact, Haettenschweiler, "Arial Narrow Bold", sans-serif;
+  background: transparent;
+  box-sizing: border-box;
+
+  background: radial-gradient(
+    163.75% 163.75% at 23.45% 140%,
+    #0025ce 0%,
+    rgba(9, 93, 230, 0.53) 100%
+  );
+  box-shadow: inset 0px 9px 15px #0d93ef;
+  border-radius: 100px;
+}
+
+.dropdown-content {
+  display: none;
+  position: absolute;
+  background-color: #092530;
+  border-radius: 10px;
+  border: 0.4px solid white;
+  min-width: 160px;
+  z-index: 1;
+}
+
+.dropdown-content a {
+  color: #ffffff;
+  padding: 12px 16px;
+  text-decoration: none;
+  display: block;
+  font-family: "Aeonik";
+  font-style: normal;
+  font-weight: 250;
+  font-size: 10px;
+  line-height: 19px;
+  
+}
+
+.dropdown-content a:hover {
+  color: #00eaff;
+}
+
+.dropdown:hover .dropdown-content {
+  display: block;
+}
+
+.dropdown:hover .dropbtn {
+  background-color: #092530;
+}
+.line {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 1px;
+  background-color: white;
+}
+</style>
+/* /* Property 1=Variant2 */ position: absolute; width: 200px; height: 33px;
+left: 20px; top: 73px; /* USA */ position: absolute; left: 0%; right: 76%; top:
+0%; bottom: 12.12%; font-family: 'Aeonik'; font-style: normal; font-weight: 250;
+font-size: 24px; line-height: 29px; /* identical to box height */ color:
+#00EAFF; /* Line 1 */ position: absolute; left: 0.5%; right: 0%; top: 100%;
+bottom: 0%; border: 0.5px solid #00EAFF; */
---
+++ pages/main.vue
@@ -0,0 +1,307 @@
+<template>
+	<header>
+		<div class="header-text">NETSOL</div>
+		<div class="button">	
+			<DropdownButton />
+		</div>
+		
+	</header>
+  <body class="background">
+    <div class="rounded-pill">
+      <div class="centertitle">News and Insights</div>
+    </div>
+    <div class="text1">Latest Insights from Leading Voices</div>
+    <div class="text2">
+      Solving the world’s problems through technology wouldn’t be possible
+      without our most important invention: the NetSol. Have a look at our
+      talented teams.
+    </div>
+    	<div class="cards">
+     	 	<div class="card">
+        		<img src="../assets/img1.png"  class="box-image" />
+        		<div class="news-tag">News</div>
+			<div class="title-card">Leasing Life Conference & Awards</div>
+			<div class="text-card">Solving the world’s problems through technology wouldn’t be possible without our most important invention.</div>
+		</div>
+      	
+      	<div class="card">
+        	<div class="card">
+        		<img src="../assets/img2.png"  class="box-image" />
+        		<div class="news-tag">News</div>
+			<div class="title-card">Auto Finance Summit 2023</div>
+			<div class="text-card">Solving the world’s problems through technology wouldn’t be possible without our most important invention.</div>
+		</div>
+      	</div>
+      	<div class="card">
+        	<div class="card">
+        		<img src="../assets/img3.png"  class="box-image" />
+        		<div class="news-tag">News</div>
+			<div class="title-card">Careers at Netsol</div>
+			<div class="text-card">Solving the world’s problems through technology wouldn’t be possible without our most important invention.</div>
+		</div>
+      	</div>
+      	<div class="card">
+        	<div class="card">
+        		<img src="../assets/img4.png"  class="box-image" />
+        		<div class="news-tag">News</div>
+			<div class="title-card">Leasing Life Conference & Awards</div>
+			<div class="text-card">Solving the world’s problems through technology wouldn’t be possible without our most important invention.</div>
+		</div>
+      	</div>
+    </div>
+    <div class="news">View All News</div>
+  </body>
+</template>
+<script>
+import DropdownButton from '../components/dropdownButton'
+</script>
+<style>
+.background {
+  position: relative;
+  width: 100%;
+  height: 100vh; /* Adjust height as needed */
+  overflow: hidden; /* Prevent child content from overflowing */
+}
+
+.background::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url("/assets/bg.jpeg");
+  background-position: center;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  filter: blur(50px); /* Adjust the blur radius as needed */
+  z-index: -1; /* Ensure the overlay is behind other content */
+   transform: scale(1.1);
+
+}
+
+.centertitle {
+  /* News and Insights */
+
+  font-family: "Aeonik";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 29px;
+  text-align: center;
+
+  text-align: center;
+  text-rendering: optimizeLegibility; /* Helps improve text rendering */
+  vertical-align: middle;
+
+  background: linear-gradient(180deg, #ffffff 49.51%, #c0c0c0 89.32%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.rounded-pill {
+  position: absolute;
+  width: 201px;
+  height: 29px;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  top: 157px;
+  display: inline-block;
+  padding: 5px 10px; /* Adjust padding as needed */
+  border-radius: 20px; /* Adjust border-radius to control the pill shape */
+  background-color: rgba(0, 0, 0, 0.548);
+  color: white; /* Set text color to contrast with the background */
+}
+.text1 {
+  /* Latest Insights from Leading Voices */
+
+  position: absolute;
+  width: 904px;
+  height: 67px;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  top: 205px;
+
+  font-family: "Aeonik";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 56px;
+  line-height: 67px;
+  text-align: center;
+
+  background: linear-gradient(180deg, #ffffff 49.51%, #c0c0c0 89.32%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+}
+.text2 {
+  position: absolute;
+  width: 1108px;
+  height: 64px;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  top: 282px;
+
+  font-family: "Aeonik";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 24px;
+  line-height: 32px;
+  /* or 133% */
+  text-align: center;
+
+  color: rgba(255, 255, 255, 0.72);
+}
+.news {
+  /* View All News */
+
+  position: absolute;
+  width: 102px;
+  height: 24px;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  top: 366px;
+
+  font-family: "Aeonik";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 24px;
+  /* identical to box height, or 150% */
+  text-decoration-line: underline;
+
+  color: #ffffff;
+}
+.cards {
+  /* Cards */
+
+  position: absolute;
+  width: 90%;
+  height:30%;
+  left: 0;
+  right: 0;
+  margin-left: auto;
+  margin-right: auto;
+  top: 471px;
+  background-color:transparent;
+  display: flex;
+}
+.card {
+  flex: 1;
+  background-color:transparent;
+  margin: 0.6%;
+  display: flex;
+  flex-direction: column;
+  position: relative; /* Add position relative */
+}
+
+.box-image {
+  /* Rectangle 532 */
+  flex: 1;
+  /* Remove position absolute */
+  width: 100%;
+  height: 60%;
+  max-height: 60%;
+  border-radius: 5px;
+ 
+}
+
+.news-tag {
+  flex: 2;
+  width: 20%;
+  height: 5%;
+  max-height:5%;
+  background-color:rgba(255, 255, 255, 0.1);
+  
+  border-radius: 5px;
+  font-size: 1.1vw;
+  margin: 2px;
+  text-align: center;
+  color: #18EBFE;
+  
+}
+.title-card{
+
+width: 100%;
+height: 20%;
+font-family: 'Aeonik';
+font-style: normal;
+font-weight: 500;
+font-size: 1.5vw;
+line-height: 2.5vw;
+
+
+
+color: rgb(241, 241, 235);
+
+flex: 3;
+order: 0;
+flex-grow: 0;
+
+}
+.text-card{
+
+width: 100%;
+height: 30%;
+
+font-family: 'Aeonik';
+font-style: normal;
+font-weight: 400;
+font-size: 1vw;
+line-height: 2vw;
+
+color: #fdfcfc;
+
+flex: 4;
+order: 1;
+flex-grow: 0;
+
+}
+.header-text{
+	/* Vector */
+
+position: absolute;
+top: 2%;
+left: 3%;
+color: #ffffff;
+font-size: 2.5vw;
+font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;
+background: transparent;
+
+}
+.button{
+position: absolute;
+
+top: 2%;
+left: 85%;
+}
+
+
+</style>
+/*
+/* Rectangle 34624148 */
+
+position: relative;
+width: 268px;
+height: 268px;
+
+
+
+background-image: url("/assets/bg.jpeg");
+  background-position: center;
+  background-attachment: fixed;
+  background-repeat: no-repeat;
+  background-size: cover;
+  
+
+*/
